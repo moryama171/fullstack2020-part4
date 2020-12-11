ced7c6f0716ffa662d1e31e3ade02583f9e7d989
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-11 15:25:23 +0100

    Extract models module

--- index.js
+++ index.js
@@ -4,15 +4,8 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-});
-
-const Blog = mongoose.model('Blog', blogSchema);
 
 const mongoUrl = process.env.MONGODB_URI;
 console.log('Connect to database', mongoUrl);
