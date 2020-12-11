5673a86a60477ba120b0adbc165401a6691fba85
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-11 15:35:05 +0100

    Refactor index  module

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
 const mongoose = require('mongoose');
 
-
 const mongoUrl = process.env.MONGODB_URI;
 console.log('Connect to database', mongoUrl);
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
@@ -17,4 +16,3 @@ app.use(express.json());
 app.use('/api/blogs', blogsRouter);
 
 module.exports = app;
-
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('./app');
-// const http = require('http');
+const http = require('http');
 
-app.listen(process.env.PORT, () => {
+const server = http.createServer(app);
+
+server.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
