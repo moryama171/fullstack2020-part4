b34ada772655ef5dac7db5a38604f958bf0c382f
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-11 15:33:37 +0100

    Extract app module

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-require('dotenv').config();
+const app = require('./app');
 // const http = require('http');
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const blogsRouter = require('./controllers/blogs');
-const mongoose = require('mongoose');
-
-
-const mongoUrl = process.env.MONGODB_URI;
-console.log('Connect to database', mongoUrl);
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/blogs', blogsRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
