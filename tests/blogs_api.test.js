56dbc79e00be5593fe67fb2e408b56066a480b46
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-14 11:08:06 +0100

    Exercise 4.10

--- tests/blogs_api.test.js
+++ tests/blogs_api.test.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const supertest = require('supertest');
 const app = require('../app');
 const Blog = require('../models/blog');
+const { initialBlogs } = require('./test_helper');
 const helper = require('./test_helper');
 const api = supertest(app);
 
@@ -35,6 +36,26 @@ describe('blogs api', () => {
     const ids = response.body.map(blog => blog.id);
     ids.map(id => expect(id).toBeDefined());
     done();
+  });
+
+  test('a new blog can be added', async (done) => {
+    await api
+      .post('/api/blogs')
+      .send(helper.newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map(b => b.title);
+    expect(titles).toContain(helper.newBlog.title);
+    done();
+
+    // test('default likes property to 0 when missing', async (done) => {
+    //   // Get
+    // });
+
   });
 
 });
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,3 +1,5 @@
+const Blog = require('../models/blog');
+
 const initialBlogs = [
   {
     title: 'React patterns',
@@ -29,15 +31,21 @@ const initialBlogs = [
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
     likes: 0,
-  },
-  {
-    title: 'Type wars',
-    author: 'Robert C. Martin',
-    url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
-    likes: 2,
   }
 ];
 
+const newBlog = {
+  title: 'Type wars',
+  author: 'Robert C. Martin',
+  url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+  likes: 2,
+};
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({});
+  return blogs.map(blog => blog.toJSON());
+};
+
 module.exports = {
-  initialBlogs
+  blogsInDb, initialBlogs, newBlog
 };
