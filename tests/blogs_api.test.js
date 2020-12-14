b8ea04b5ada6b00a46a2f8f35232ccc7859069bc
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-14 09:58:24 +0100

    Test get all route

---
+++ tests/blogs_api.test.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+const helper = require('./test_helper');
+const api = supertest(app);
+
+beforeEach(async () => {
+  await Blog.deleteMany({});
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog));
+  const promiseArray = blogObjects.map(blog => blog.save());
+  await Promise.all(promiseArray);
+});
+
+describe('blogs api', () => {
+
+  test('blogs are returned as json', async (done) => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+    done();
+  });
+
+  test('all blogs are returned', async (done) => {
+    const response = await api.get('/api/blogs');
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+    done();
+  });
+
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
