cce593ea0e5a1d5284efe08934c48c797684ceba
moryama <5599992-moryama@users.noreply.gitlab.com>
2020-12-17 16:26:15 +0100

    Implement create new user

---
+++ controllers/users.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt');
+const usersRouter = require('express').Router();
+const User = require('../models/user');
+
+
+usersRouter.post('/', async (request, response) => {
+  const body = request.body;
+
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+
+  const user = new User ({
+    username: body.username, 
+    password: passwordHash,
+    name: body.name
+  });
+
+  const savedUser = await user.save();
+  response.status(201).json(savedUser);
+});
+
+module.exports = usersRouter;
